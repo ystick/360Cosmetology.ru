b9841c01bd9890ebfc7f1303cfe50822f5d035f0
Ystick <ykhanzhin@gmail.com>
2025-09-17 06:43:43 +0300

    drop Certificate link in menu

--- src/app/assets/data/navigation/menu.navigation.ts
+++ src/app/assets/data/navigation/menu.navigation.ts
@@ -1,13 +1,9 @@
-import { ABONEMENTS_LINK, ABOUT_US_LINK, ACTIONS_LINK, CERTIFICATE_LINK, BLOG_LINK, CONTACTS_LINK, Link, NEWS_LINK, RESULTS_LINK_FOTO, SERVICES_PRICING_LINK, STAFF_LINK } from '@constants/route.constants'
+import { ABONEMENTS_LINK, ABOUT_US_LINK, ACTIONS_LINK, BLOG_LINK, CONTACTS_LINK, Link, NEWS_LINK, RESULTS_LINK_FOTO, SERVICES_PRICING_LINK, STAFF_LINK } from '@constants/route.constants'
 
 export const menuNavigation: Link[] = [
   {
     href: SERVICES_PRICING_LINK.href,
     value: SERVICES_PRICING_LINK.value
-  },
-  {
-    href: CERTIFICATE_LINK.href,
-    value: CERTIFICATE_LINK.value
   },
   {
     href: ACTIONS_LINK.href,
